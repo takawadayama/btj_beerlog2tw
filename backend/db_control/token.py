fecd751185c29b52609d871e0dc71d841c76b218
N-Hiro0123 <n.hiroki0909@gmail.com>
2024-08-06 01:37:12 +0900

    JWTを使った認証まで確認

--- backend/db_control/token.py
+++ backend/db_control/token.py
@@ -33,7 +33,7 @@ class Token(BaseModel):
 
 
 class TokenData(BaseModel):
-    username: str | None = None
+    user_id: int | None = None
 
 
 class UserInDB(User):
@@ -48,12 +48,16 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def get_user(db, user_mail: str):
+def get_user_by_mail(db, user_mail: str):
     return db.query(User).filter(User.user_mail == user_mail).first()
 
 
+def get_user_by_id(db, user_id: str):
+    return db.query(User).filter(User.user_id == user_id).first()
+
+
 def authenticate_user(db, user_mail: str, user_password: str):
-    user = get_user(db, user_mail)
+    user = get_user_by_mail(db, user_mail)
     if not user:
         return False
     if not verify_password(user_password, user.user_password):
@@ -66,13 +70,13 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=60)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 
-async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+def get_current_user_id(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -80,16 +84,15 @@ async def get_current_user(db: Session = Depends(get_db), token: str = Depends(o
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
+        user_id = int(payload.get("sub"))
+
+        if user_id is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+
     except JWTError:
         raise credentials_exception
-    user = get_user(db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
+
+    return user_id
 
 
 @router.post("/token", response_model=Token)
@@ -102,22 +105,20 @@ async def login_for_access_token(form_data: LoginRequest, db: Session = Depends(
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.user_id}, expires_delta=access_token_expires)
+
+    # JWTに入れる値はstrにしておく
+    access_token = create_access_token(data={"sub": str(user.user_id)}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
 
 @router.get("/users/me/", response_model=UserModel)  # UserModelをレスポンスモデルとして使用
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
+async def read_users_me(current_user_id: int = Depends(get_current_user_id)):
+    return current_user_id
 
 
 # パスワードの更新エンドポイント
 @router.put("/update_password/")
 def update_user_password(request: UpdatePasswordRequest, db: Session = Depends(get_db)):
-    # print(user_id)
-    # print(new_password)
-    print(request.user_id)
-    print(request.new_password)
 
     user = db.query(User).filter(User.user_id == request.user_id).first()
     if user is None:
@@ -129,3 +130,8 @@ def update_user_password(request: UpdatePasswordRequest, db: Session = Depends(g
     user.user_password = hashed_password
     db.commit()
     return {"message": "Password updated successfully"}
+
+
+@router.get("/test_jwt/")
+def get_test_jwt(user_id: int = Depends(get_current_user_id)):
+    return user_id
